2372f072527271949556df4e72b3cdf8ef093607
Runi <Runiniclasen@hotmail.com>
2021-03-28 14:34:46 +0200

    task4 finished

--- Assignment2/task4/MapReduce.js
+++ Assignment2/task4/MapReduce.js
@@ -58,16 +58,55 @@ console.log('4.5 ', result);
 
 //6. Use reduce to sum the array of numbers
 list = [1, 2, 3, 4, 5];
-result = list.reduce() //continue here
-console.log('4.6 ', result); //result here
+result = list.reduce((accumulator, currentValue) => accumulator += currentValue);
+console.log('4.6 ', result); //15
+
 
 //7. Use reduce to sum the x-value of the objects in the array
-list =  [{x: 1},{x: 2}, {x: 3}];
-result = list.reduce() //continue here
-console.log('4.7 ', result); //result here
+list = [{ x: 1 }, { x: 2 }, { x: 3 }];
+result = list.reduce((total, current) => total += current.x, 0); //start total as int 0
+console.log('4.7 ', result); //6
 
-//8
 
-//9
+//8. Use reduce to flatten an array of arrays
+list = [[1, 2], [3, 4], [5, 6]];
+result = list.reduce((total, current) => total.concat(current), []) //start total as new array
+console.log('4.8 ', result); //[ 1, 2, 3, 4, 5, 6 ]
 
-//10
+
+//9. Use reduce to return an array of the positive numbers
+list = [-3, -1, 2, 4, 5];
+result = list.reduce((total, current) => current > 0 ? total.concat(current) : total, []) //add to array if positive, else return array as is (for next function loop)
+console.log('4.9 ', result); //[ 2, 4, 5 ]
+
+
+//10. The accumulator function can obviously use objects outside of itself.  Use reduce to implement groupBy. 
+let people = [
+  { name: 'Rikke', age: 46 },
+  { name: 'Michael', age: 47 },
+  { name: 'Mathias', age: 46 }
+];
+
+//The expected outcome is an array of people grouped by their age
+let groupBy = (objectArray, property) => {
+  return objectArray.reduce(function (acc, obj) {
+    let key = obj[property] //people[index].age
+    if (!acc[key]) { //newPeople[age] 
+      acc[key] = [] //(if not exists, add)
+    }
+    acc[key].push(obj) //push current into grouped array
+    return acc //return for reduce function
+  }, {}) //start acc as empty obj
+};
+
+//Assignment expects groupedPeople to be 46: Rikke, Mathias & 47: Michael
+
+let groupedPeople = groupBy(people, 'age'); //group people by key 'age'.
+console.log('4.10 ', groupedPeople);
+
+/*
+{
+  '46': [ { name: 'Rikke', age: 46 }, { name: 'Mathias', age: 46 } ],
+  '47': [ { name: 'Michael', age: 47 } ]
+}
+*/
