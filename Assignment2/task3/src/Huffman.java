6cae5570611c2cec8e2de1ae9535f84fde59a26e
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-04-02 16:13:37 +0200

    huffman
    heavily inspired by https://www.techiedelight.com/huffman-coding/

---
+++ Assignment2/task3/src/Huffman.java
@@ -0,0 +1,134 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Huffman {
+    Node root;
+    StringBuilder strB;
+
+    private void buildTree(String text){
+        if (text == null || text.length() == 0) {
+            return;
+        }
+
+        Map<Character, Integer> frequencies = new HashMap<>();
+        for (char letter: text.toCharArray()) {
+            frequencies.put(letter, frequencies.getOrDefault(letter, 0) + 1);
+        }
+
+        PriorityQueue<Node> queue;
+        queue = new PriorityQueue<>(Comparator.comparingInt(node -> node.frequency));
+
+        for (Map.Entry<Character, Integer> entry : frequencies.entrySet()) {
+            queue.add(new Node(entry.getKey(), entry.getValue()));
+        }
+
+        while (queue.size() != 1) {
+            // Remove the two nodes of the highest priority (the lowest frequency) from the queue
+            Node left = queue.poll();
+            Node right = queue.poll();
+
+            // create a new node with these two nodes as children and with a frequency
+            // equal to the sum of both and add the new node to the priority queue.
+            int sum = left.frequency + right.frequency;
+            queue.add(new Node(null, sum, left, right));
+        }
+        // pointer to the root of Huffman Tree
+        root = queue.peek();
+    }
+
+    public Map encode(String text){
+        buildTree(text);
+        Map<Character, String> huffmanCodes = new HashMap<>();
+        _encode(root, "", huffmanCodes);
+
+        System.out.println("\n\nOriginal string is:\n" + text + "\n");
+        System.out.println("The codes are:\n" + huffmanCodes + "\n");
+
+        strB = new StringBuilder();
+        for (char letter: text.toCharArray()) {
+            strB.append(huffmanCodes.get(letter));
+        }
+
+        System.out.println("The encoded string is:\n" + strB+ "\n");
+        return huffmanCodes;
+    }
+
+    private void _encode(Node root, String str, Map<Character, String> huffmanCode) {
+        if (root == null) {
+            return;
+        }
+
+        if (isLeafNode(root)) {
+            huffmanCode.put(root.letter, str.length() > 0 ? str : "1");
+        }
+
+        _encode(root.left, str + '0', huffmanCode);
+        _encode(root.right, str + '1', huffmanCode);
+    }
+
+    public void decode(String text, Map huffmanCodes){
+        _encode(root, "", huffmanCodes);
+        strB = new StringBuilder();
+        strB.append(text);
+
+        System.out.println("The decoded string is:");
+
+        if (isLeafNode(root)) {
+            // Special case: For input like a, aa, aaa, etc.
+            while (root.frequency-- > 0) {
+                System.out.print(root.letter);
+            }
+        }
+        else {
+            // Traverse tree again and decode the encoded string
+            int index = -1;
+            while (index < text.length() - 1) {
+                index = _decode(root, index, strB);
+            }
+        }
+    }
+
+    private int _decode(Node root, int index, StringBuilder sb) {
+        if (root == null){
+            return index;
+        }
+
+        if (isLeafNode(root)){
+            System.out.print(root.letter);
+            return index;
+        }
+
+        index++;
+
+        root = (sb.charAt(index) == '0') ? root.left : root.right;
+        index = _decode(root, index, sb);
+        return index;
+    }
+
+    public boolean isLeafNode(Node node){
+        return node.left == null && node.right == null;
+    }
+
+}
+
+class Node {
+    Character letter;
+    Integer frequency;
+    Node left = null;
+    Node right = null;
+
+    Node(Character letter, Integer frequency) {
+        this.letter = letter;
+        this.frequency = frequency;
+    }
+
+    public Node(Character letter, Integer frequency, Node left, Node right) {
+        this.letter = letter;
+        this.frequency = frequency;
+        this.left = left;
+        this.right = right;
+    }
+
+}
--- Assignment2/task3/src/Main.java
+++ Assignment2/task3/src/Main.java
@@ -1,2 +1,16 @@
+import java.util.Map;
+
 public class Main {
+    public static void main(String[] args) {
+        Huffman huffman = new Huffman();
+        String text1 = "beebs beepps!!!!! their eerie ears hear pears";
+        Map codes = huffman.encode(text1);
+        String code = "0110101001101111110011010101110111011110000000000000001100011000111100010010110101001000101011010011101011111100011110011101011011101001110101111";
+        huffman.decode(code, codes);
+
+        System.out.println("\n\n*********************************************************");
+        String text2 = "pete is here";
+        Map codes2 = huffman.encode(text2);
+
+    }
 }
