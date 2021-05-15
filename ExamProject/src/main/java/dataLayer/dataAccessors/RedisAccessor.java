0eebbb9008e10a0dd59e099e838683f775836f2d
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-05-15 15:26:47 +0200

    simple redis connection

--- ExamProject/src/main/java/dataLayer/dataAccessors/RedisAccessor.java
+++ ExamProject/src/main/java/dataLayer/dataAccessors/RedisAccessor.java
@@ -1,4 +1,14 @@
 package dataLayer.dataAccessors;
+import redis.clients.jedis.Jedis;
+
 
 public class RedisAccessor {
+    Jedis jedis;
+    // docker run --name redistwo -v redis-data:/data -p 6379:6379 -d redis:alpine (porten er vigtig! uden -p "bindes" den ikke til localhost og kan ikke findes fra windows)
+    public RedisAccessor(){
+        jedis = new Jedis("0.0.0.0", 6379);
+    }
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("0.0.0.0", 6379);
+    }
 }
