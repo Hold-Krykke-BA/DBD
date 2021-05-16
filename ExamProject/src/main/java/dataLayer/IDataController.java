9ca6ea151495ff6e9b273bd2b157ddcb28c30f14
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-05-16 15:34:14 +0200

    prep

--- ExamProject/src/main/java/dataLayer/IDataController.java
+++ ExamProject/src/main/java/dataLayer/IDataController.java
@@ -1,4 +1,17 @@
 package dataLayer;
 
+import models.dataModels.FPitem;
+import models.dataModels.SubReddit;
+import models.dataModels.User;
+
+import java.util.List;
+
 public interface IDataController {
+    public List<FPitem> getFrontPageItems(String userID);
+    public List<SubReddit> getSubRedditsByUser(String userID);
+    public User getUserInfo(String userID);
+    public void getUserMessages(String userID);
+    public void createMessage(String userIDsender, String userIDreciever);
+    public void authenticateUser(String userID);
+    // and more...
 }
--- ExamProject/src/main/java/dataLayer/controller/DataControllerImpl.java
+++ ExamProject/src/main/java/dataLayer/controller/DataControllerImpl.java
@@ -1,4 +1,64 @@
 package dataLayer.controller;
 
-public class DataControllerImpl {
+import dataLayer.IDataController;
+import dataLayer.dataAccessors.Neo4jAccessor;
+import dataLayer.dataAccessors.PostgresAccessor;
+import dataLayer.dataAccessors.RedisAccessor;
+import models.dataModels.FPitem;
+import models.dataModels.SubReddit;
+import models.dataModels.User;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataControllerImpl implements IDataController {
+    RedisAccessor redDBD;
+    PostgresAccessor pgrDBD;
+    Neo4jAccessor neoDBD;
+    int minFrontpageItems = 25;
+
+    public DataControllerImpl(){
+        this.redDBD = new RedisAccessor();
+        this.pgrDBD = new PostgresAccessor();
+        this.neoDBD = new Neo4jAccessor();
+
+    }
+
+    @Override
+    public List<FPitem> getFrontPageItems(String userID) {
+        List<FPitem> result = redDBD.getFPitems(userID);
+        if (result.size() < minFrontpageItems){
+            List<FPitem> uncached = new ArrayList<>();
+            // call postgress and add new FPitems to uncached list
+            redDBD.createCacheID(userID);
+            redDBD.createMultiplePostCache(uncached);
+        }
+        return result;
+    }
+
+    @Override
+    public List<SubReddit> getSubRedditsByUser(String userID) {
+        return null;
+    }
+
+    @Override
+    public User getUserInfo(String userID) {
+        return null;
+    }
+
+    @Override
+    public void getUserMessages(String userID) {
+
+    }
+
+    @Override
+    public void createMessage(String userIDsender, String userIDreciever) {
+
+    }
+
+    @Override
+    public void authenticateUser(String userID) {
+
+    }
 }
