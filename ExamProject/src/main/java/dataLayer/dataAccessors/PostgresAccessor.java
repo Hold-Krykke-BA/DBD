910c83c47067c57c91a2b9f123f90b7bf86d7f98
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-05-18 22:39:48 +0200

    java test connection and data returned

--- ExamProject/src/main/java/dataLayer/dataAccessors/PostgresAccessor.java
+++ ExamProject/src/main/java/dataLayer/dataAccessors/PostgresAccessor.java
@@ -1,9 +1,7 @@
 package dataLayer.dataAccessors;
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class PostgresAccessor {
@@ -13,6 +11,9 @@ public class PostgresAccessor {
     public PostgresAccessor(){
         connection = connectToDB("jdbc:postgresql://localhost:5433/soft2021");
 
+    }
+    public Connection getConnection(){
+        return connection;
     }
 
     private Connection connectToDB(String url){
@@ -28,8 +29,19 @@ public class PostgresAccessor {
         return connection;
     }
 
-    public static void main(String[] args) {
+    public void test(Connection conn) throws SQLException {
+        DatabaseMetaData dbmd = conn.getMetaData();
+        try (ResultSet tables = dbmd.getTables(null, null, "%", new String[] { "TABLE" })) {
+            while (tables.next()) {
+                System.out.println(tables.getString("TABLE_NAME"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) throws SQLException {
         PostgresAccessor pgr = new PostgresAccessor();
+        pgr.test(pgr.getConnection());
 
     }
 }
