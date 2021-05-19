6c9af027d8811e12c3b89641bdac0f24a4df99c7
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-05-19 22:40:35 +0200

    rnd string

--- ExamProject/src/main/java/util/StringManipulation.java
+++ ExamProject/src/main/java/util/StringManipulation.java
@@ -3,9 +3,9 @@ package util;
 import java.util.Random;
 
 public class StringManipulation {
+    static final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
     public static String generateRandomString(int targetStringLength) {
-        final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
         Random random = new Random();
         StringBuilder strB = new StringBuilder(targetStringLength);
             for(int i = 0; i < targetStringLength; i++)
