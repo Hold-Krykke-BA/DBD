fa5de7a860ae4545b48a9e7d290b1063b21c785b
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-05-31 10:06:55 +0200

    basic working spring app

---
+++ ExamSpringApplication/src/test/java/holdkrykke/exam/ExamSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package holdkrykke.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
